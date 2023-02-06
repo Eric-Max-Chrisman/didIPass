370935a46c84f9c899104c6eb20cf80e09ff7b4f
ChrismanE21 <54912180+ChrismanE21@users.noreply.github.com>
2023-02-06 05:50:57 -0600

    worng error code fix

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 function placeHolder(req: Request, res: Response): void {
   console.log('placeHolder');
-  res.sendStatus(201);
+  res.sendStatus(501);
 }
 
 export { placeHolder };
